a2dfe47068bd191e7312cec805b87613f6141a84
NYX <nyx.0g0p.sub@gmail.com>
2023-05-27 05:46:28 +0000

    add column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -12,13 +12,15 @@
                 <tr>
                     <th>id</th>
                     <th>タスク</th>
+                    <th>ステータス</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                     <tr>
-                        <td><a class="link link-hover text-info" href="#">{{ $task->id }}</a></td>
+                        <td><a class="link link-hover text-info" href="{{ route('tasks.show', $task->id) }}">{{ $task->id }}</a></td>
                         <td>{{ $task->content }}</td>
+                        <td>{{ $task->status }}</td>
                     </tr>
                 @endforeach
             </tbody>
